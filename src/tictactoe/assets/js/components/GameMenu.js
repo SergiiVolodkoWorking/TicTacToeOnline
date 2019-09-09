332c55a75f96198860878f63ab553d9b1e54096f
Sergii Volodko <svolodko@8thlight.com>
2019-09-09 11:44:28 +0100

    Added bootstrap via CDN

--- src/tictactoe/assets/js/components/GameMenu.js
+++ src/tictactoe/assets/js/components/GameMenu.js
@@ -1,5 +1,8 @@
 import * as React from 'react'
 
-const GameMenu = () => <div><h2>Game Menu will be here</h2></div>
+const GameMenu = () => <div>
+        <h2>Game Menu will be here</h2>
+        <button class="btn btn-lg btn-success">Start VS Easy bot</button>
+    </div>
 
 export default GameMenu;
