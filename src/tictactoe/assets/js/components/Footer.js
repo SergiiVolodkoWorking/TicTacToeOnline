9007bde2dbeb6d6bfd1c07100bd82656d6314eed
Sergii Volodko <svolodko@8thlight.com>
2019-09-09 14:20:08 +0100

    Minor UI cleanup

--- src/tictactoe/assets/js/components/Footer.js
+++ src/tictactoe/assets/js/components/Footer.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import 'bootstrap'
 
 class Footer extends React.Component {
     constructor() {
--- src/tictactoe/assets/js/components/GameMenu.js
+++ src/tictactoe/assets/js/components/GameMenu.js
@@ -1,8 +1,11 @@
 import * as React from 'react'
 
-const GameMenu = () => <div>
+const GameMenu = () => 
+    <div>
         <h2>Game Menu will be here</h2>
-        <button id='start-easy-btn' className="btn btn-lg btn-success"><i className="fas fa-play-circle"></i> Start VS Easy bot</button>
+        <button id='start-easy-btn' className="btn btn-lg btn-success">
+            <i className="fas fa-play-circle"></i> Start VS Easy bot
+        </button>
     </div>
 
 export default GameMenu;
