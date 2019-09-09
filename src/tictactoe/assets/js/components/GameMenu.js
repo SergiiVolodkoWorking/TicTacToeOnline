4f4ab5ec3471de5eefe36f39f187823970056c5a
Sergii Volodko <svolodko@8thlight.com>
2019-09-09 15:11:16 +0100

    UI - bootstrap setup corrections

--- src/tictactoe/assets/js/components/GameMenu.js
+++ src/tictactoe/assets/js/components/GameMenu.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 const GameMenu = () => 
     <div>
         <h2>Game Menu will be here</h2>
-        <button id='start-easy-btn' className="btn btn-lg btn-success">
+        <button id='start-easy-btn' className="btn-lg btn-success">
             <i className="fas fa-play-circle"></i> Start VS Easy bot
         </button>
     </div>
