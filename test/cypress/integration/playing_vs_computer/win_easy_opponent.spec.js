823a0435f1e8c76da95d3c2bba0575e9dca3a9a8
Sergii Volodko <svolodko@8thlight.com>
2019-09-09 11:02:47 +0100

    Preparing form start-game implementation

---
+++ test/cypress/integration/playing_vs_computer/win_easy_opponent.spec.js
@@ -0,0 +1,13 @@
+describe('Win easy opponent', function(){
+    it('happy path', function() {
+        cy.visit('/')
+
+        // see start menu
+        // don't see board
+
+        // click start button
+
+        // no longer see start menu
+        // see game board
+  })
+})
--- test/cypress/integration/version/versioning.spec.js
+++ test/cypress/integration/version/versioning.spec.js
@@ -1,7 +1,7 @@
 describe('Versioning', function() {
-    it('is visible on the start page', function() {
-      cy.visit('/')
-      var expected = '0.11'
-      cy.get('#version').should('contain', expected)
-    })
+  it('is visible on the start page', function() {
+    cy.visit('/')
+    var expected = '0.11'
+    cy.get('#version').should('contain', expected)
   })
+})
