b3f43a0d51b3b14ed9b2941d93d396c4db26710c
Sergii Volodko <svolodko@8thlight.com>
2019-09-18 17:33:04 +0100

    Updated version to 0,12

--- src/tictactoe/lib/tictactoe_web/controllers/version_controller.ex
+++ src/tictactoe/lib/tictactoe_web/controllers/version_controller.ex
@@ -4,7 +4,7 @@ defmodule TictactoeWeb.VersionController do
   action_fallback TictactoeWeb.FallbackController
 
   def index(conn, _params) do
-    result = %{version: "0.11"}
+    result = %{version: "0.12"}
     json(conn, result)
   end
 end
--- test/cypress/integration/version/versioning.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Versioning', function() {
-  it('is visible on the start page', function() {
-    cy.visit('/')
-    cy.get('#version').should('contain', '0.11')
-  })
-})
