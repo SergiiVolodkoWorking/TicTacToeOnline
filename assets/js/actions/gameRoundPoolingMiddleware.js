98936201346cd856acc349c57d661646519f7f85
Sergii Volodko <svolodko@8thlight.com>
2019-10-01 00:28:05 +0100

    UI: minor cleanup

--- assets/js/actions/gameRoundPoolingMiddleware.js
+++ assets/js/actions/gameRoundPoolingMiddleware.js
@@ -4,19 +4,17 @@ import { GameRoundState, GameEvents } from '.'
 let interval = null;
 
 export const gameRoundPoolingMiddleware = function (store) {
-    // Called when calling applyMiddleware so
-    // our middleware can have access to the store
-
     const updateRound = () => {
         const currentRoundState = store.getState().tictactoeFSM.round_state;
-
-        if (currentRoundState === GameRoundState.WAITING_UPDATE) {
-            const game_id = store.getState().tictactoeFSM.game_id;
-            makeGetRequest(
-                'api/game/' + game_id,
-                data => store.dispatch({ type: GameEvents.ROUND_STATE_FETCHED, game_round: data })
-            );
+        if (currentRoundState !== GameRoundState.WAITING_UPDATE) {
+            return;
         }
+
+        const game_id = store.getState().tictactoeFSM.game_id;
+        makeGetRequest(
+            'api/game/' + game_id,
+            data => store.dispatch({ type: GameEvents.ROUND_STATE_FETCHED, game_round: data })
+        );
     };
 
     if (!interval) {
