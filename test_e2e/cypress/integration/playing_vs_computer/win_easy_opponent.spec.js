18f63c4f758222796262268e69192399259e2506
Sergii Volodko <svolodko@8thlight.com>
2019-10-02 19:13:42 +0100

    CI/CD: fixing cypress tests - upload of snapshots is working

--- test_e2e/cypress/integration/playing_vs_computer/win_easy_opponent.spec.js
+++ test_e2e/cypress/integration/playing_vs_computer/win_easy_opponent.spec.js
@@ -2,16 +2,8 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand();
 
 describe('Win easy opponent', function(){
-  before(() => {
-    Cypress.config({
-      viewportWidth: 1000,
-      viewportHeight: 790,
-    })
-  })
-
   it('Opens the game', function() {
     cy.visit('/')
-    cy.viewport(1000, 749)
     cy.matchImageSnapshot('Expected Start Menu');
   })
 
@@ -27,23 +19,23 @@ describe('Win easy opponent', function(){
 
   it('Makes his first move', function() {
     cy.get('#space-0').click()
-    cy.wait(1000)
+    cy.wait(600)
     cy.matchImageSnapshot('Expected First move');
   })
 
   it('Makes remaining moves to win ', function() {
-    cy.wait(1000)
+    cy.wait(600)
     cy.get('#space-3').click()
-    cy.wait(1000)
+    cy.wait(600)
 
     cy.get('#space-4').click()
-    cy.wait(1000)
+    cy.wait(600)
 
     cy.get('#space-7').click()
-    cy.wait(1000)
+    cy.wait(600)
 
     cy.get('#space-8').click()
-    cy.wait(1000)
+    cy.wait(600)
 
     cy.matchImageSnapshot('Expected Victory screen');
   })
