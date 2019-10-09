74cbf1787e63bf0c2048d51ec39a0b2582310cf5
Sergii Volodko <svolodko@8thlight.com>
2019-10-09 10:31:43 +0100

    Version increased to 0.8

--- lib/tictactoe_web/controllers/version_controller.ex
+++ lib/tictactoe_web/controllers/version_controller.ex
@@ -4,7 +4,7 @@ defmodule TictactoeWeb.VersionController do
   action_fallback TictactoeWeb.FallbackController
 
   def index(conn, _params) do
-    result = %{version: "0.12"}
+    result = %{version: "0.8"}
     json(conn, result)
   end
 end
